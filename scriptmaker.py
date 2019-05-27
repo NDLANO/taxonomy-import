e83f200477b54563d3170ec396159026b06c5e59
Øystein Øvrebø <oystein.ovrebo@bouvet.no>
2019-05-27 17:19:18 +0200

    added 4 subjects

--- scriptmaker.py
+++ scriptmaker.py
@@ -15,7 +15,7 @@ import shutil
 
 #   SET THESE VALUES before running - must point to existing folders
 #   filesystem root (needed because Ubuntu on Windows...)
-fs_root = "/mnt/c/"
+fs_root = "/c/"
 
 #   folder where TSV input files are stored
 input_folder = "ndla-spoling/"
@@ -56,20 +56,25 @@ export_folder = "ndla-export/"
 #   24  https://docs.google.com/spreadsheets/d/1TldbFJRmIIcVCBFUO5F1Ae8NJHyrUy0pN6n-D8wZr50/  helse-og-oppvekst
 #   25  https://docs.google.com/spreadsheets/d/1dHnhasaBYDoF9768FLlHajt0hnp0KoEpxrCrYiNzZ1A/  ikt-servicefag
 #
-#   26 https://docs.google.com/spreadsheets/d/1XGiUf6GdYbIgOPYZ4iLDyIttOPKD43BPKpy526ELq8A/   kroppsøving
-#   27 https://docs.google.com/spreadsheets/d/17YfYge4G8KFXeLDC0BJCfdjBlWeYDYIlIsSM1GlxYDY/   internasjonal engelsk
-#   28 https://docs.google.com/spreadsheets/d/1F5jj5287R57Z-709bfFYBKykr5W-JYoO3fxMctlvoGs/   teknikk og industriell produksjon
-#   29 https://docs.google.com/spreadsheets/d/1imO2qaSVbQLpq24cnt8rzlZSMr7tigdDOHr--AMyt_Q/   praktisk matematikk
-#   30 https://docs.google.com/spreadsheets/d/1EhmDHgeaS76dvSeFCV2dVLbMpps3FLN1gwaMsRMmtn0/   Matematikk for yrkesfaglige programmer
-#   31 https://docs.google.com/spreadsheets/d/1p5LtP-nSpp4L81nRL6uThDOPftb41h9rUdVt5rb8VJ8/   Matematikk for samfunnsfag
-#   32 https://docs.google.com/spreadsheets/d/1fs8zarUb28VnzRsdarO6D0u45eRaI-LAn82ez18ng1o/   Matematikk for realfag
-#   33 https://docs.google.com/spreadsheets/d/1dtLKlULCspVKxXod3TEzjiJRtNPBFvPWAG2qq1HxKEw/   1T - Matematikk fellesfag
-#   34 https://docs.google.com/spreadsheets/d/1t49dUUQFnrAKnmq8xftIzHkMbY08fQ9i7b-1lhi6CVc/   1P - Matematikk fellesfag
-#   35 https://docs.google.com/spreadsheets/d/1UtiPGBPS-h3VUdUejPRJjk0Mi1yad4SOHXjwUYxC024/   Reiseliv
-#   36 https://docs.google.com/spreadsheets/d/1ny0gSl57scuev-MCs7kXBmdKj-eEJqqYP4NuP0CI7LA/   Transport og logistikk
-#   37 https://docs.google.com/spreadsheets/d/1H4EBhvVpaK9tnHdagm7X54YR_orYgg2A4Ij70SvSiF8/   Restaurant- og matfag Vg1
-#   38 https://docs.google.com/spreadsheets/d/1BfP-RZ3NfhWjO87nFe6JqcdfBG2wK6zvbCTt_5MExu0/   Design og håndverk Vg1
-#   39 https://docs.google.com/spreadsheets/d/1vk9-mASASr-UpPKy-N_08EcDRd1EpD1_2nVaq-fe_bM/   Engelsk Vg1
+#   26  https://docs.google.com/spreadsheets/d/1XGiUf6GdYbIgOPYZ4iLDyIttOPKD43BPKpy526ELq8A/  kroppsøving
+#   27  https://docs.google.com/spreadsheets/d/17YfYge4G8KFXeLDC0BJCfdjBlWeYDYIlIsSM1GlxYDY/  internasjonal engelsk
+#   28  https://docs.google.com/spreadsheets/d/1F5jj5287R57Z-709bfFYBKykr5W-JYoO3fxMctlvoGs/  teknikk og industriell produksjon
+#   29  https://docs.google.com/spreadsheets/d/1imO2qaSVbQLpq24cnt8rzlZSMr7tigdDOHr--AMyt_Q/  praktisk matematikk
+#   30  https://docs.google.com/spreadsheets/d/1EhmDHgeaS76dvSeFCV2dVLbMpps3FLN1gwaMsRMmtn0/  Matematikk for yrkesfaglige programmer
+#   31  https://docs.google.com/spreadsheets/d/1p5LtP-nSpp4L81nRL6uThDOPftb41h9rUdVt5rb8VJ8/  Matematikk for samfunnsfag
+#   32  https://docs.google.com/spreadsheets/d/1fs8zarUb28VnzRsdarO6D0u45eRaI-LAn82ez18ng1o/  Matematikk for realfag
+#   33  https://docs.google.com/spreadsheets/d/1dtLKlULCspVKxXod3TEzjiJRtNPBFvPWAG2qq1HxKEw/  1T - Matematikk fellesfag
+#   34  https://docs.google.com/spreadsheets/d/1t49dUUQFnrAKnmq8xftIzHkMbY08fQ9i7b-1lhi6CVc/  1P - Matematikk fellesfag
+#   35  https://docs.google.com/spreadsheets/d/1UtiPGBPS-h3VUdUejPRJjk0Mi1yad4SOHXjwUYxC024/  Reiseliv
+#   36  https://docs.google.com/spreadsheets/d/1ny0gSl57scuev-MCs7kXBmdKj-eEJqqYP4NuP0CI7LA/  Transport og logistikk
+#   37  https://docs.google.com/spreadsheets/d/1H4EBhvVpaK9tnHdagm7X54YR_orYgg2A4Ij70SvSiF8/  Restaurant- og matfag Vg1
+#   38  https://docs.google.com/spreadsheets/d/1BfP-RZ3NfhWjO87nFe6JqcdfBG2wK6zvbCTt_5MExu0/  Design og håndverk Vg1
+#   39  https://docs.google.com/spreadsheets/d/1vk9-mASASr-UpPKy-N_08EcDRd1EpD1_2nVaq-fe_bM/  Engelsk Vg1
+#   40  https://docs.google.com/spreadsheets/d/15oLpaisgmSKHjjK_6wKHuccVRMHpPGf_MwlykTeWjZE/  BUA-faget
+#   41  https://docs.google.com/spreadsheets/d/17QSXFo0qguVNkykZRP5QsdStdWl2oY2TLlGLES9DPvk/  Kokk- og servitørfaget VG2
+#   42  https://docs.google.com/spreadsheets/d/1pcWpSDSARt9M64Fo35S7uLm1cc2oabwWshi1miS3xBA/  Biologi
+#   43  https://docs.google.com/spreadsheets/d/1gYPHFkA0Ji4qYhbW41hUTlOcvOatFcoYDJKXr3C7Kng/  Sosiologi og sosialantropologi
+#   44  https://docs.google.com/spreadsheets/d/1iO2wjB_AeFep0lmhSfWrEK3rGWRGqP_D46COV06o9ek/  Religion og etikk
 
 jar_location = "target/taxonomy-import.jar"
 
@@ -131,7 +136,12 @@ subjects = [
     ("transport-og-logistikk", "Transport og logistikk"),
     ("restaurant-og-matfag", "Restaurant- og matfag Vg1"),
     ("design-og-håndverk", "Design og håndverk Vg1"),
-    ("engelsk", "Engelsk Vg1")
+    ("engelsk", "Engelsk Vg1"),
+    ("bua-faget","BUA-faget"),
+    ("kokk-og-servitør-faget", "Kokk- og servitørfag Vg2"),
+    ("biologi", "Biologi"),
+    ("sosiologi-og-sosialantropologi", "Sosiologi og sosialantropologi"),
+    ("religion-og-etikk", "Religion og etikk")
 ]
 
 
@@ -167,7 +177,7 @@ def create_shell_scripts():
                 logfile = '{}{}-{}-{}-{}.log'.format(path, id, subject[0], date, server[0])
                 environment = server[1]
                 script.write(
-                    "cat {} | {} -i {} -n {} -e {} &> {}\n".format(input_file, jar_location, subject_id, subject_name,
+                    "cat {} | {} -i {} -ci CLIENT_ID -cs CLIENT_SECRET -ts TOKEN_SERVER -n {} -e {} &> {}\n".format(input_file, jar_location, subject_id, subject_name,
                                                                   environment, logfile))
             script.close()
 
